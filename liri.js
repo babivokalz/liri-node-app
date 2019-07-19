aaa838e8461ab3998551262918fafe6d23a7b91f
AlaCrity <wlchen1105@gmail.com>
2019-07-18 23:19:02 -0400

    craeted functions for spotify, write to file, and read file as well as writing the switch,case usage for the commands

--- liri.js
+++ liri.js
@@ -14,4 +14,57 @@ var fs = require('fs');
 let writeToFile = false;
 let input = process.argv.slice(2).join(' ');
 
-input = 
+// input = commandInput(input);
+// let action = input.slice(0, input.indexOf(' ') == -1 ? input.length : input.indexOf(' '));
+// let value = input.indexOf(' ') == -1 ? null : input.slice(input.indexOf(' ') + 1); 
+
+useLiri (action, value);
+
+function useLiri (action, value) {
+    switch (action) {
+        case "concert-this":
+            queryBandsInTown(value);
+            break;
+        case "spotify-this-song":
+            querySpotify(value);
+            break;
+        case "movie-this":
+            queryOMDB(value);
+            break;
+        case "do-what-it-says":
+            readFile('random.txt');
+            break;
+    }
+};
+
+function readFile(filename) {
+    fs.readFile(filename, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        data = data.split(',');
+        useLiri(data[0], data[1]);
+    });
+};
+
+function writeToFile(filename, output) {
+    fs.appendFile(filename, output, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+};
+
+function querySpotify(song) {
+    var songName;
+    if (song === undefined || song === null || song === '') {
+        songName = ""
+    }
+}
+
+// function commandInput(input) {
+//     if (input.indexOf('-') != -1 && input.indexOf('-') == 0) {
+//         let flag = input.slice(0,2);
+
+//     }
+// }
