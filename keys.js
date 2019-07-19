7bf67053f3bfd5128e40b22b214f06e60be4bc9c
AlaCrity <wlchen1105@gmail.com>
2019-07-18 22:46:20 -0400

    created majority of the initial files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
+
+85585bceeb4f4181980a124ef33435c1
+
+c099a80829ec41a4ae7d7cd9ccef5176
